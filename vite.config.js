29b1ecc3512c61814940774ad187eefb3553a015
lenakononchuk <lena@rockingtech.co.uk>
2024-07-19 13:04:35 +0300

    vite-plugin-imagemin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,30 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Imagemin from 'vite-plugin-imagemin';
+
+
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Imagemin({
+      gifsicle: {
+        optimizationLevel: 3,
+      },
+      optipng: {
+        optimizationLevel: 5,
+      },
+      mozjpeg: {
+        quality: 75,
+      },
+      webp: {
+        quality: 75,
+      },
+      avif: {
+        quality: 50,
+      },
+    }),
+  ],
   base: '/test_assignment/',
 
   server: {
